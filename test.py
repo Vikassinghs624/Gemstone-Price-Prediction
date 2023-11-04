d39ef025cf0357a7b50f6d43f3771828af89eaa3
Vikassinghs624 <vikassinghsikarwar1996@gmail.com>
2023-11-04 12:08:20 +0530

    final file has been updated

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-import os
+# import os
 
-path="notebooks/research.ipynb"
+# path="notebooks/research.ipynb"
 
-dir,file=os.path.split(path)
+# dir,file=os.path.split(path)
 
-os.makedirs(dir,exist_ok=True)
+# os.makedirs(dir,exist_ok=True)
 
-with open(path,"w") as f:
-    pass
+# with open(path,"w") as f:
+#     pass
+
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
+customdata=CustomData(1.52,62.2,58.0,7.27,7.33,4.55,'Premium','F','VS2')
+df=customdata.get_data_as_dataframe()
+print(df)
